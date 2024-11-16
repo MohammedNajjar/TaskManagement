229d8d8b5027823a15a257f2232b0a10237f0351
MohammedNajjar <najjarmohammed085@gmail.com>
2024-11-16 13:40:51 +0200

    Entities

---
+++ TaskManagement.Api/Entities/Category.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TaskManagement.Api.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public int CategoryId { get; set; }
+    public User? User { get; set; }
+    public int UserId { get; set; }
+    public required ICollection<Task> Tasks { get; set; }
+}
---
+++ TaskManagement.Api/Entities/Task.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace TaskManagement.Api.Entities;
+
+public class Task
+{
+
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool IsCompleted { get; set; }
+    public int Priority { get; set; }
+    public int UserId { get; set; }
+    public User? User { get; set; }
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
+
+}
